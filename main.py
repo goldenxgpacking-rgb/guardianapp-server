3583445a1f551225acd15eebd2acb74b888e98dc
goldenxgpacking-rgb <goldenxgpacking@mail.com>
2026-06-12 00:02:08 +0630

    add /health endpoint for Railway port check

--- main.py
+++ main.py
@@ -234,5 +234,9 @@ setInterval(refresh, 5000);
 </script></body></html>"""
     return Response(html, mimetype='text/html')
 
+@app.route('/health')
+def health():
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
